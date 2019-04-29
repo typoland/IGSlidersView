84c6c2318931e81539bc98948664670278a3d9ce
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-04-29 10:03:33 +0200

    Initial Commit

---
+++ IGSlidersView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  IGSlidersView
+//
+//  Created by Łukasz Dziedzic on 29/04/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
