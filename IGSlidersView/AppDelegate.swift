373419739ac81b13c247ef757cb8e21a26794d79
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-05-01 12:29:26 +0200

    Pozamieniane controllery, wszystko jest teraz w osobnym frameworku

--- IGSlidersView/AppDelegate.swift
+++ IGSlidersView/AppDelegate.swift
@@ -7,15 +7,21 @@
 //
 
 import Cocoa
+import IGSliders
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var editViewController:IGSlidersController!
+    @IBOutlet weak var editView: NSView!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        editViewController.insertViewInto(editView)
+
+//        editView.addSubview(editViewController.view)
+//        editViewController.view.frame = NSMakeRect(0, 0, editView.frame.width, editView.frame.height)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
